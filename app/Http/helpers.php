3cefc6cc9e06f0a9feb31817c4106f92d2af746c
Ibraheem Adeniyi <ibonly01@gmail.com>
2019-08-12 17:31:11 -0500

    Modify enivronment helper

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,10 +1,11 @@
 <?php
+
 if (!function_exists('load_asset')) {
     /*
      * Set where to load assets from based on secure or non secure http
      */
     function load_asset($assetUrl)
     {
-        return (env('APP_ENV') === 'PRODUCTION') ? secure_asset($assetUrl) : asset($assetUrl);
+        return (env('APP_ENV') == 'PRODUCTION') ? secure_asset($assetUrl) : asset($assetUrl);
     }
 }
